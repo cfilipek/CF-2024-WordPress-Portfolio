96b1955c414dd5d4c3553c3fb4a81c86969f61a2
Claire Filipek <clairefilipek@claires-mbp.myfiosgateway.com>
2023-12-08 17:24:23 -0500

    added/styled contact me page

---
+++ parts/page-contact.php
@@ -0,0 +1,17 @@
+<?php
+	/**
+	 * Template part for displaying about content
+	 */
+
+?>
+
+<section class="section section-contact">
+	<div class="grid-container grid-x">
+		<div class="cell small-12 medium-10 medium-offset-1">
+            <h2 class="title">Contact Me</h2>
+            <div class="the-content">
+                <?php the_content() ?>
+            </div>
+        </div>
+    </div>
+</section>   
