deb56e7c97ec1378b64f7e311c572c1b3d5ef4d7
Claire Filipek <clairefilipek@claires-mbp.myfiosgateway.com>
2023-12-08 17:18:10 -0500

    created/styled About page

---
+++ parts/page-about.php
@@ -0,0 +1,17 @@
+<?php
+	/**
+	 * Template part for displaying about content
+	 */
+
+?>
+
+<section class="section section-about">
+	<div class="grid-container grid-x">
+		<div class="cell small-12 medium-10 medium-offset-1">
+            <h2 class="title">About Me</h2>
+            <div class="the-content">
+                <?php the_content() ?>
+            </div>
+        </div>
+    </div>
+</section>   
